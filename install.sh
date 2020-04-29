c74b6f99f8dbe38c2bf43a41b90dfa25e87da09c
HuJyunKai <contact@sivilization.com>
2020-04-29 17:39:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,6 +93,15 @@ service cockpit start
 
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/HuJK/Code-Server-Hub/master/install2.sh)"
 docker pull whojk/code-server-hub-docker
-
+docker run -d -p 9000:9000 \
+       --name portainer --restart always \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v portainer_data:/data \
+       -v /etc/letsencrypt:/etc/letsencrypt \
+       -v /etc/code-server-hub/cert/:/etc/code-server-hub/cert/ \
+       portainer/portainer \
+       --ssl \
+       --sslcert /etc/code-server-hub/cert/ssl.pem \
+       --sslkey  /etc/code-server-hub/cert/ssl.key
 
 exit 0
