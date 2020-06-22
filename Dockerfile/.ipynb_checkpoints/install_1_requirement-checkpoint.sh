8b09b34cac3f10ca9c9e3cd1c9b84ae10754dfc4
Your Name <you@example.com>
2020-06-23 02:32:58 +0800

    install order bug

--- Dockerfile/.ipynb_checkpoints/install_1_requirement-checkpoint.sh
+++ Dockerfile/.ipynb_checkpoints/install_1_requirement-checkpoint.sh
@@ -8,7 +8,7 @@ apt-get -y install software-properties-common
 add-apt-repository universe
 apt-get -y update
 apt-get -y dist-upgrade
-apt-get -y install apt-utils runit locales openssh-server autossh cron vim git sudo rsync nginx-full socat apache2-utils wget curl git ca-certificates python3 python3-pip python3-dev python-setuptools p7zip-full p7zip-rar git-core 
+apt-get -y install apt-utils runit locales openssh-server autossh cron vim git sudo rsync nginx-full socat apache2-utils wget curl git ca-certificates python3 python3-pip python3-dev python-setuptools p7zip-full p7zip-rar git-core zsh tmux
 
 pip3       install --upgrade torch torchaudio tensorflow-gpu tensorboard tensorboardX scipy numpy virtualenv virtualenvwrapper matplotlib torchvision jupyter jupyterlab jupyterhub jupyter_http_over_ws setuptools
 
@@ -21,14 +21,10 @@ jupyter serverextension enable jupyter_tensorboard --sys-prefix
 apt-get -y autoremove ; apt-get autoclean
 
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ;  ~/.fzf/install
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 mkdir ~/.virtualenvs
 rm -rf /var/lib/apt/lists/* ; localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 ; locale-gen en_US.UTF-8
 
-echo "thefuck --alias | source" >> /etc/fish/config.fish 
 
 mkdir -p /etc/code-server-hub/.cshub
 cd /etc/code-server-hub
