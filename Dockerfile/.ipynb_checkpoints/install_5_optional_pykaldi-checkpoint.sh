cc145b17ee1d717624f8b56963a6428da99510f4
Your Name <you@example.com>
2020-06-23 13:55:32 +0800

    test

--- Dockerfile/.ipynb_checkpoints/install_5_optional_pykaldi-checkpoint.sh
+++ Dockerfile/.ipynb_checkpoints/install_5_optional_pykaldi-checkpoint.sh
@@ -4,6 +4,10 @@ set -e
 
 git clone --depth 1 https://github.com/pykaldi/pykaldi.git /usr/share/pykaldi
 cd  /usr/share/pykaldi/tools
+
+ln -s /usr/bin/python3 /usr/local/bin/python
+ln -s pip3 /usr/bin/pip
+
 bash check_dependencies.sh python3
 bash install_protobuf.sh python3
 bash install_clif.sh  python3
@@ -11,6 +15,10 @@ bash install_kaldi.sh pyhonn3
 
 cd  /usr/share/pykaldi
 python3 setup.py install
+
+
+rm /usr/local/bin/python
+rm /usr/bin/pip
 
 #delete self
 rm /tmp/install3.sh
