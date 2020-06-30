f6b2f02d585f51ec0d965bfa0af7f119b0875f97
Your Name <you@example.com>
2020-06-30 15:18:26 +0800

    install.sh test

--- install.sh
+++ install.sh
@@ -25,7 +25,8 @@ else
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
                 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
                 apt-get update;
-                apt-get install -y docker-ce docker-ce-cli containerd.io;;
+                apt-get install -y docker-ce docker-ce-cli containerd.io;
+                break;;
             [Aa]* ) 
                 echo "Aborted";
                 exit;;
@@ -56,7 +57,8 @@ else
                     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -;
                     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list;
                     sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit;
-                    systemctl restart docker;;
+                    systemctl restart docker;
+                    break;;
                 [Aa]* ) 
                     echo "Aborted";
                     exit;;
