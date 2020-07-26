72283dca1335deca2bfd027bf829cafd145d3c63
Your Name <you@example.com>
2020-07-26 19:13:13 +0800

    minimal version

--- install.sh
+++ install.sh
@@ -112,7 +112,7 @@ if [ "$1" == "docker" ]; then
                         distribution=$(. /etc/os-release;echo $ID$VERSION_ID);
                         curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -;
                         curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list;
-                        sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit;
+                        sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime;
                         systemctl restart docker;
                         break;;
                     [Aa]* ) 
