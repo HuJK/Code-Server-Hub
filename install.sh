1d17b71c00740af3f514a38345f091fc108f0741
Your Name <you@example.com>
2020-07-28 02:22:41 +0800

    aarch64 test

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ if [ "$1" == "docker" ]; then
                 [Yy]* ) 
                     apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
                     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+                    sudo add-apt-repository "deb [arch=${cpu_arch}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
                     apt-get update;
                     apt-get install -y docker-ce docker-ce-cli containerd.io;
                     break;;
