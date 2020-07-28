a8c14e77ab1ab6ac2b56ca89ab30ea4484bd90a3
Your Name <you@example.com>
2020-07-28 20:56:46 +0800

    include chainer in basicML

--- Dockerfile/install_3_optional_basicML.sh
+++ Dockerfile/install_3_optional_basicML.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 ln -s cuda /usr/local/nvidia
 ln -s libcudart.so.10.2 /usr/local/cuda/lib64/libcudart.so.10.1
-pip3       install --upgrade opencv-python librosa mxnet torch torchaudio tensorflow-gpu tensorboard tensorboardX torchvision  
+pip3       install --upgrade opencv-python librosa mxnet torch torchaudio tensorflow-gpu tensorboard tensorboardX torchvision chainer
 
 jupyter labextension install jupyterlab_tensorboard
 pip3 install jupyter_tensorboard
