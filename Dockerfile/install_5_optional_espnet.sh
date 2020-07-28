7f4d5431dd59790985c6b85a024e5115cb3efa47
Your Name <you@example.com>
2020-07-28 21:32:50 +0800

    fix espnet

--- Dockerfile/install_5_optional_espnet.sh
+++ Dockerfile/install_5_optional_espnet.sh
@@ -30,12 +30,12 @@ ln -s /usr/bin/python3 /usr/local/bin/python
 ln -s pip3 /usr/bin/pip
 cd /usr/share/espnet/tools
 alias pip=pip3
-rm -rf venv; mkdir -p venv/bin; touch venv/bin/activate  # Create an empty file
-make KALDI=/usr/share/kaldi-asr PYTHON=dummy
-cd /usr/share/espnet/tools
 sed -i.bak "/^PYTHON_VERSION := .*/cPYTHON_VERSION := 3.6.9" Makefile
 sed -i.bak "/^CHAINER_VERSION := .*/cCHAINER_VERSION := 7.4.0" Makefile
 sed -i.bak "/^TH_VERSION := .*/cTH_VERSION := 1.5.1" Makefile
+rm -rf venv; mkdir -p venv/bin; touch venv/bin/activate  # Create an empty file
+make KALDI=/usr/share/kaldi-asr PYTHON=dummy
+cd /usr/share/espnet/tools
 make check_install
 
 
