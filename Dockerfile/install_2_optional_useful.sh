39ea13c99a4ce8e1d5d4130aefcc59f7b5738ff2
Your Name <you@example.com>
2020-07-29 16:02:54 +0800

    install.sh standard

--- Dockerfile/install_2_optional_useful.sh
+++ Dockerfile/install_2_optional_useful.sh
@@ -6,10 +6,10 @@ echo "Install additional tools"
 
 apt-get -y update
 
-apt-get -y install fish htop aria2 lsof tree ncdu golang default-jdk atop duplicity emacs gawk gnupg2 lftp libsqlite3-dev libssl-dev libtool mc mtr netcat parallel screen silversearcher-ag sl sqlite3 tig vifm wyrd zlib1g-dev zlib1g-dev openssh-server autossh socat libopenblas-dev liblapack-dev gfortran
+apt-get -y install fish htop aria2 lsof tree ncdu golang default-jdk atop duplicity emacs gawk gnupg2 lftp libsqlite3-dev libssl-dev libtool mc mtr netcat parallel screen silversearcher-ag sl sqlite3 tig vifm wyrd zlib1g-dev zlib1g-dev openssh-server autossh socat libopenblas-dev liblapack-dev gfortran cmake
 apt-get -y autoremove ; apt-get autoclean
 
-pip3       install --upgrade tornado tqdm sympy galgebra pandas plotly nose pillow pyparsing ninja scikit-image scikit-learn scipy matplotlib              
+pip3       install --upgrade tornado tqdm sympy galgebra pandas plotly nose pillow pyparsing scikit-image scikit-learn scipy matplotlib              
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ;  ~/.fzf/install
 echo "thefuck --alias | source" >> /etc/fish/config.fish 
