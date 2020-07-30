f60d025752bafa863cc98c36eb2158bdc3b4701f
Your Name <you@example.com>
2020-07-30 10:25:45 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -145,7 +145,7 @@ if [ "$1" == "docker" ]; then
         }
     else
         echo "Nvidia driver not found, skip nvidia-docker autoinstall"
-        if [ "$1" == "standard" ]; then
+        if [ "$2" == "standard" ]; then
             docker pull whojk/code-server-hub-docker:standard
             sed -i.bak "/^image_name_cpu = .*/cimage_name_cpu = 'whojk/code-server-hub-docker:standard'" /etc/code-server-hub/util/create_docker.py
         else
