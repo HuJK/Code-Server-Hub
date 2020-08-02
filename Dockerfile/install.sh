ed28e88013ab33cd0faa4fa107d8b15df2005bc5
Your Name <you@example.com>
2020-08-02 18:03:37 +0800

    fix heroku no home var

--- Dockerfile/install.sh
+++ Dockerfile/install.sh
@@ -5,7 +5,7 @@ apt-get update
 #apt-get upgrade -y
 echo "###install dependanse phase###"
 echo "Install dependances"
-apt-get install -y nginx-extras ca-certificates socat rsync fish build-essential
+apt-get install -y nginx-extras ca-certificates socat rsync fish build-essential shellcheck apt-utils gdb
 apt-get install -y tmux libncurses-dev htop nodejs npm wget sudo curl vim openssl git
 apt-get install -y python3 python3-pip python3-dev p7zip-full 
 pip3 install jupyterlab speedtest-cli pylint
---
+++ Dockerfile/move2docker/root/example_code/armstrong.js
@@ -0,0 +1,33 @@
+// program to find Armstrong number between intervals
+
+// take an input
+let lowNumber = 8
+let highNumber = 500
+
+console.log ('Armstrong Numbers:');
+
+// looping through lowNumber to highNumber
+for (let i = lowNumber; i <= highNumber; i++) {
+
+    // converting number to string 
+    let numberOfDigits = i.toString().length;
+
+    let sum = 0;
+
+    // create a temporary variable
+    let temp = i;
+
+    /* loop through a number to find if 
+    a number is an Armstrong number */
+    while (temp > 0) {
+
+        let remainder = temp % 10;
+        sum += remainder ** numberOfDigits;
+        // removing last digit from the number
+        temp = parseInt(temp / 10); // convert float into integer
+    }
+ 
+    if (sum == i) {
+        console.log(i);
+    }
+}
