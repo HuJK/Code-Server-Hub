a5b619c6405402f0ac2d24a3a311cb6e2c10ebd6
Your Name <you@example.com>
2020-08-05 01:07:42 +0800

    fix py

--- Dockerfile/move2docker/root/example_code/factors.py
+++ Dockerfile/move2docker/root/example_code/factors.py
@@ -2,10 +2,10 @@
 
 # This function computes the factor of the argument passed
 def print_factors(x):
-   print("The factors of",x,"are:")
-   for i in range(1, x + 1):
-       if x % i == 0:
-           print(i)
+    print("The factors of",x,"are:")
+    for i in range(1, x + 1):
+        if x % i == 0:
+            print(i)
 
 num = 320
 
