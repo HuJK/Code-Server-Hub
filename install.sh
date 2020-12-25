1b7505efcf0ce2dfbac0ae2081fe430fb3dbef27
Your Name <you@example.com>
2020-12-25 13:36:22 +0800

    install.sh update

--- install.sh
+++ install.sh
@@ -1,4 +1,71 @@
 #!/bin/bash
+
+
+HOMEPGE="NO"
+HOMEPGE_SSL="NO"
+JUPYTERHUB="ASK"
+JUPYTERHUB_PIP3="ASK"
+COCKPIT="ASK"
+DOCKER="ASK"
+DOCKER_NVIDIA="ASK"
+DOCKER_PORTAINER="ASD"
+DOCKER_IMAGE="standard"
+
+
+for i in "$@"
+do
+case $i in
+    -hp=*|--homepage=*)
+    HOMEPGE="${i#*=}"
+    shift # past argument=value
+    ;;
+    -hps=*|--homepage-ssl=*)
+    HOMEPGE_SSL="${i#*=}"
+    shift # past argument=value
+    ;;
+    -jph=*|--jupyterhub=*)
+    JUPYTERHUB="${i#*=}"
+    shift # past argument=value
+    ;;
+    -jphp3=*|--jupyterhub-pip3=*)
+    JUPYTERHUB_PIP3="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--cockpit=*)
+    COCKPIT="${i#*=}"
+    shift # past argument=value
+    ;;
+    -d=*|--docker=*)
+    DOCKER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -dn=*|--docker-nvidia=*)
+    DOCKER_NVIDIA="${i#*=}"
+    shift # past argument=value
+    ;;
+    -dp=*|--docker-portainer=*)
+    DOCKER_PORTAINER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -di=*|--docker-image=*)
+    DOCKER_IMAGE="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    ;;
+esac
+done
+
+echo "Install homepage             = ${HOMEPGE}"
+echo "Enable SSL for homepage      = ${HOMEPGE_SSL}"
+echo "Install jupyterhub           = ${JUPYTERHUB}"
+echo "Install pip3 for jupyterhub  = ${JUPYTERHUB_PIP3}"
+echo "Install cockpit              = ${COCKPIT}"
+echo "Install docker               = ${DOCKER}"
+echo "Install nvidia-docker        = ${DOCKER_NVIDIA}"
+echo "Install portainer            = ${DOCKER_PORTAINER}"
+echo "Docker image for code-server = ${DOCKER_IMAGE}"
+
 set -e
 #echo "###update phase###"
 apt-get update
@@ -11,14 +78,13 @@ apt-get install -y python3 python3-pip python3-dev p7zip-full libffi-dev nodejs
 set +e # folling command only have one will success
 #cockpit for user management
 apt-get install -y npm
-apt-get install -y -t xenial-backports cockpit cockpit-pcp #for ubuntu 16.04
-apt-get install -y -t bionic-backports cockpit cockpit-pcp #for ubuntu 18.04
-apt-get install -y cockpit cockpit-pcp                     #for ubuntu 20.04
+
 set -e
 
 function get_cpu_architecture()
 {
-    local cpuarch=$(uname -m)
+    local cpuarch;
+    cpuarch=$(uname -m)
     case $cpuarch in
          x86_64)
               echo "amd64";
@@ -83,23 +149,107 @@ cd /etc/code-server-hub/cert
 openssl genrsa -out ssl.key 2048
 openssl req -new -x509 -key ssl.key -out ssl.pem -days 3650 -subj /CN=localhost
 
-set +e
-mv /var/www/html/index.nginx-debian.html   /var/www/html/index.nginx-debian.html.bak
-set -e
+if [[ ! $HOMEPGE =~ [yYnN].* ]]; then
+    read -p "Do you want to install homepage(yes/no)? " HOMEPGE
+fi
+if [[ $HOMEPGE =~ [yY].* ]]; then
+    set +e
+    mv /var/www/html/index.nginx-debian.html   /var/www/html/index.nginx-debian.html.bak
+    set -e
+fi
 
-if [ "$1" == "docker" ] || [ "$1" == "autoInstallDocker" ]; then
+if [[ ! $HOMEPGE_SSL =~ [yYnN].* ]]; then
+    read -p "Do you want to install self singed ssl for homepage(yes/no)? " HOMEPGE_SSL
+fi
+if [[ $HOMEPGE_SSL =~ [yY].* ]]; then
+    #ask for enable ssl at nginx
+    if ! grep -q -e  "^[^#]*listen 443 ssl" /etc/nginx/sites-available/default; then
+        while true; do
+            echo "=========================================================================="
+            if [ "$4" == "ssl" ]; then
+                yn="Yes"
+            elif [ "$4" == "nossl" ]; then
+                yn="No"
+            else
+                read -p "Do you want enable ssl encryption on your nginx config /etc/nginx/sites-available/default ? (Yes/No/Abort)" yn
+            fi
+            case $yn in
+                [Yy]* ) 
+                    sed -i.bak "/^[^#]*listen 80.*/a\  listen 443 ssl;\n  listen [::]:443 ssl;\n  ssl_certificate '/etc/code-server-hub/cert/ssl.pem';\n  ssl_certificate_key '/etc/code-server-hub/cert/ssl.key';" /etc/nginx/sites-available/default;
+                    break;;
+                [Nn]* ) 
+                    echo "Skipped";
+                    break;;
+                [Aa]* ) 
+                    echo "Aborted";
+                    exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
+fi
+# Cockpit
+if [[ ! $COCKPIT =~ [yYnN].* ]]; then
+    read -p "Do you want to install cockpit at 9090 now(yes/no)? " COCKPIT
+fi
+if [[ $COCKPIT =~ [yY].* ]]; then
+    apt-get install -y -t xenial-backports cockpit cockpit-pcp #for ubuntu 16.04
+    apt-get install -y -t bionic-backports cockpit cockpit-pcp #for ubuntu 18.04
+    apt-get install -y cockpit cockpit-pcp                     #for ubuntu 20.04
+fi
+
+#Jupyterhub
+if [[ ! $JUPYTERHUB =~ [yYnN].* ]]; then
+    read -p "Do you want to install jupyterhub at port 8000(yes/no)? " JUPYTERHUB
+fi
+if [[ $JUPYTERHUB =~ [yY].* ]]; then
+    { # try
+        pip3 -V
+    } || { # catch
+        # save log for exception 
+        echo "=========================================================================="
+        while true; do
+            if [[ ! $JUPYTERHUB_PIP3 =~ [yYnN].* ]]; then
+                read -p "pip3 has problem, trying to fix now?? (Yes/No/Abort)" JUPYTERHUB_PIP3
+            fi
+            
+            case $JUPYTERHUB_PIP3 in
+                [Yy]* ) 
+                    apt purge -y python3-pip
+                    wget https://bootstrap.pypa.io/get-pip.py
+                    python3 get-pip.py
+                    break;;
+                [Aa]* ) 
+                    echo "Aborted";
+                    exit;;
+                [Nn]* ) 
+                    echo "Skipped";
+                    break;;
+                * ) echo "Please answer yes or no or abort.";;
+            esac
+        done
+    }
+    set +e
+    pip3 install certbot-dns-cloudflare
+    wget -O- https://raw.githubusercontent.com/HuJK/Code-Server-Hub/master/install_jupyterhub.sh | bash
+    set -e
+fi
+
+#Jupyterhub
+if [[ ! $DOCKER =~ [yYnN].* ]]; then
+    read -p "Do you want to install docker version of code-server-hub at port 2087(yes/no)? " DOCKER
+fi
+if [[ $DOCKER =~ [yY].* ]]; then
     #ask for install docker
     if hash docker 2>/dev/null; then
         echo "Docker installed, skip docker auto install"
     else
         echo "=========================================================================="
         while true; do
-            if [ "$1" == "autoInstallDocker" ]; then
-                yn="Yes"
-            else
-                read -p "Docker not detected. Dou you want to install docker now? (Yes/No/Abort)" yn
+            if [[ ! $DOCKER_INSTALL =~ [yYnN].* ]]; then
+                read -p "Docker not detected. Dou you want to install docker now? (Yes/No/Abort)" DOCKER_INSTALL
             fi
-            case $yn in
+            case $DOCKER_INSTALL in
                 [Yy]* ) 
                     apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
                     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
@@ -118,18 +268,66 @@ if [ "$1" == "docker" ] || [ "$1" == "autoInstallDocker" ]; then
         done
     fi
     usermod -aG docker www-data
+
+
+    #Portainer
+    has_portainer=$(docker container ls -a | grep portainer) || true
+    PASSWORD=""
+    if [ -z "$has_portainer" ]; then
+        echo "=========================================================================="
+        
+        while true; do
+        if [[ ! $DOCKER_PORTAINER =~ [yYnN].* ]]; then
+            read -p "Do you want install portainer(a web based docker gui) at port 9000 now? (Yes/No)" DOCKER_PORTAINER
+        fi
+            case $DOCKER_PORTAINER in
+                [Yy]* ) 
+                    docker run -d -p 9000:9000 \
+                        --name portainer --restart always \
+                        -v /var/run/docker.sock:/var/run/docker.sock \
+                        -v portainer_data:/data \
+                        -v /etc/letsencrypt:/etc/letsencrypt \
+                        -v /etc/code-server-hub/cert/:/etc/code-server-hub/cert/ \
+                        portainer/portainer \
+                        --ssl \
+                        --sslcert /etc/code-server-hub/cert/ssl.pem \
+                        --sslkey  /etc/code-server-hub/cert/ssl.key;
+                    echo "=========================================================================="
+                    PASSWORD=`date +%s|md5sum|base64|head -c 12`
+                    echo "Your username:password for portainer is admin:${PASSWORD} Login at https://$(wget -qO- https://ifconfig.me/):9000"
+                    echo "Generated password are store at ~/.ssh/portainer_pwd.txt"
+                    echo "admin:${PASSWORD}" > ~/.ssh/portainer_pwd.txt
+                    n=0
+                    until [ $n -ge 16 ]; do
+                        curl 'https://127.0.0.1:9000/api/users/admin/init' --data-binary '{"Username":"admin","Password":"'"${PASSWORD}"'"}' --insecure && break
+                        n=$((n + 1))
+                        echo "Retry ${n}/16"
+                        sleep 1
+                    done
+                    break;;
+                [Nn]* ) 
+                    echo "Skipped";
+                    break;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
+
     #ask for install nvidia-docker
     if hash nvidia-smi 2>/dev/null; then
         docker pull whojk/code-server-hub-docker:basicML
         { # try
             docker run --rm --gpus all nvidia/cuda:10.2-base nvidia-smi &&
-            echo "Nvidia docker installed, skip  nvidia-docker autoinstall"
+            echo "Nvidia docker installed, skip nvidia-docker autoinstall"
         } || { # catch
             # save log for exception 
             echo "=========================================================================="
             while true; do
-                read -p "Nvidia-docker not detected. Dou you want to install nvidia-docker now? (Yes/No/Abort)" yn
-                case $yn in
+
+                if [[ ! $DOCKER_NVIDIA =~ [yYnN].* ]]; then
+                    read -p "Nvidia-docker not detected. Dou you want to install nvidia-docker now? (Yes/No/Abort)" DOCKER_NVIDIA
+                fi
+                case $DOCKER_NVIDIA in
                     [Yy]* ) 
                         # Nvidia-Docker
                         distribution=$(. /etc/os-release;echo $ID$VERSION_ID);
@@ -150,56 +348,14 @@ if [ "$1" == "docker" ] || [ "$1" == "autoInstallDocker" ]; then
         }
     else
         echo "Nvidia driver not found, skip nvidia-docker autoinstall"
-        if [ "$2" == "standard" ]; then
+        if [[ $DOCKER_IMAGE == "standard" ]]; then
             docker pull whojk/code-server-hub-docker:standard
             sed -i.bak "/^image_name_cpu = .*/cimage_name_cpu = 'whojk/code-server-hub-docker:standard'" /etc/code-server-hub/util/create_docker.py
         else
             docker pull whojk/code-server-hub-docker:minimal
         fi
     fi
-    #Portainer
-    has_portainer=$(docker container ls -a | grep portainer) || true
-    PASSWORD=""
-    while [ -z "$has_portainer" ]; do
-        echo "=========================================================================="
-        if [ "$3" == "portainer" ]; then
-            yn="Yes"
-        elif [ "$3" == "noportainer" ]; then
-            yn="No"
-        else
-            read -p "Do you want install portainer(a web based docker gui) now? (Yes/No)" yn
-        fi
-        case $yn in
-            [Yy]* ) 
-                docker run -d -p 9000:9000 \
-                    --name portainer --restart always \
-                    -v /var/run/docker.sock:/var/run/docker.sock \
-                    -v portainer_data:/data \
-                    -v /etc/letsencrypt:/etc/letsencrypt \
-                    -v /etc/code-server-hub/cert/:/etc/code-server-hub/cert/ \
-                    portainer/portainer \
-                    --ssl \
-                    --sslcert /etc/code-server-hub/cert/ssl.pem \
-                    --sslkey  /etc/code-server-hub/cert/ssl.key;
-                echo "=========================================================================="
-                PASSWORD=`date +%s|md5sum|base64|head -c 12`
-                echo "Your username:password for portainer is admin:${PASSWORD} Login at https://$(wget -qO- https://ifconfig.me/):9000"
-                echo "Generated password are store at ~/.ssh/portainer_pwd.txt"
-                echo "admin:${PASSWORD}" > ~/.ssh/portainer_pwd.txt
-                n=0
-                until [ $n -ge 16 ]; do
-                    curl 'https://127.0.0.1:9000/api/users/admin/init' --data-binary '{"Username":"admin","Password":"'"${PASSWORD}"'"}' --insecure && break
-                    n=$((n + 1))
-                    echo "Retry ${n}/16"
-                    sleep 1
-                done
-                break;;
-            [Nn]* ) 
-                echo "Skipped";
-                break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+
     #install code-hub-docker
     cd /etc/code-server-hub
     set +e
@@ -212,59 +368,9 @@ else
 fi
 
 
-#ask for enable ssl at nginx
-if ! grep -q -e  "^[^#]*listen 443 ssl" /etc/nginx/sites-available/default; then
-    while true; do
-        echo "=========================================================================="
-        if [ "$4" == "ssl" ]; then
-            yn="Yes"
-        elif [ "$4" == "nossl" ]; then
-            yn="No"
-        else
-            read -p "Do you want enable ssl encryption on your nginx config /etc/nginx/sites-available/default ? (Yes/No/Abort)" yn
-        fi
-        case $yn in
-            [Yy]* ) 
-                sed -i.bak "/^[^#]*listen 80.*/a\  listen 443 ssl;\n  listen [::]:443 ssl;\n  ssl_certificate '/etc/code-server-hub/cert/ssl.pem';\n  ssl_certificate_key '/etc/code-server-hub/cert/ssl.key';" /etc/nginx/sites-available/default;
-                break;;
-            [Nn]* ) 
-                echo "Skipped";
-                break;;
-            [Aa]* ) 
-                echo "Aborted";
-                exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-fi
 
-{ # try
-    pip3 -V
-} || { # catch
-    # save log for exception 
-    echo "=========================================================================="
-    while true; do
-        read -p "pip3 has problem, trying to fix now?? (Yes/No/Abort)" yn
-        case $yn in
-            [Yy]* ) 
-                apt purge -y python3-pip
-                wget https://bootstrap.pypa.io/get-pip.py
-                python3 get-pip.py
-                break;;
-            [Aa]* ) 
-                echo "Aborted";
-                exit;;
-            [Nn]* ) 
-                echo "Skipped";
-                break;;
-            * ) echo "Please answer yes or no or abort.";;
-        esac
-    done
-}
-set +e
-pip3 install certbot-dns-cloudflare
-wget -O- https://raw.githubusercontent.com/HuJK/Code-Server-Hub/master/install2.sh | bash
-set -e
+
+
 echo "###restart nginx and cockpit###"
 systemctl enable nginx
 systemctl enable cockpit.socket
