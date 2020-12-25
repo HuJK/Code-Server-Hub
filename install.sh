536a21e315693e4ee67c9a8c4cc466b918b22947
Your Name <you@example.com>
2020-12-25 14:06:08 +0800

    install.sh for debian test2

--- install.sh
+++ install.sh
@@ -285,7 +285,7 @@ if [[ $DOCKER =~ [yY].* ]]; then
             case $DOCKER_INSTALL in
                 [Yy]* ) 
                     apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
-                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+                    curl -fsSL "https://download.docker.com/linux/$(echo $OS | tr '[:upper:]' '[:lower:]')/gpg" | sudo apt-key add -;
                     sudo add-apt-repository "deb [arch=${cpu_arch}] https://download.docker.com/linux/$(echo $OS | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable";
                     apt-get update;
                     apt-get install -y docker-ce docker-ce-cli containerd.io;
