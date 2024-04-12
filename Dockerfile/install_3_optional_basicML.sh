88614b6c7f4fc5d41c418acf295dc8ca6a67ddff
Your Name <you@example.com>
2024-04-12 16:47:06 +0800

    upgrade torch, fix aqlm_gpu require py309

--- Dockerfile/install_3_optional_basicML.sh
+++ Dockerfile/install_3_optional_basicML.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 export DEBIAN_FRONTEND=noninteractive
-
+. /etc/os-release
 function get_cpu_architecture()
 {
     local cpuarch=$(uname -m)
@@ -24,9 +24,8 @@ export NODE_OPTIONS=--openssl-legacy-provider
 cpu_arch=$(get_cpu_architecture)
 apt update 
 ln -s cuda /usr/local/nvidia
-apt-get install ffmpeg libsm6 libxext6  -y
-aria2c  -x 16 -j 16  "https://ollama.com/download/ollama-linux-$cpu_arch" -d / -o /usr/bin/ollama
-chmod 755 /usr/bin/ollama
+apt-get install ffmpeg libsm6 libxext6 libhdf5-dev  -y
+
 eval "$(/opt/miniconda/bin/conda shell.bash hook)"
 conda activate base
 
@@ -43,16 +42,17 @@ case $CUDA_VERSION in
     ;;
 esac
 
+
 if [ "$cpu_arch" = "amd64" ]; then
     echo "These packages are x86_64 only."
     pip3 install onnxruntime-gpu
-    pip3 install --upgrade triton aqlm[gpu,cpu]
+
     case $CUDA_VERSION in
     11.2)
         pip3 install torch==1.12.0+cu102 torchaudio==0.12.0+cu102 torchvision==0.13.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
         ;;
     11.8)
-        pip3 install torch==2.0.1+cu118 torchaudio==2.0.2+cu118 torchvision==0.15.2+cu118 -f https://download.pytorch.org/whl/torch_stable.html
+        pip3 install torch==2.2.2+cu118 torchaudio==2.2.2+cu118 torchvision==0.17.2+cu118 -f https://download.pytorch.org/whl/torch_stable.html
         pip3 install nvidia-tensorrt bitsandbytes
         ;;
     *)
@@ -60,12 +60,17 @@ if [ "$cpu_arch" = "amd64" ]; then
         exit 255
         ;;
     esac
+    pip3 install onnxruntime-gpu
+    if dpkg --compare-versions "$VERSION_ID" "<=" "20.04"; then
+        pip3 install --upgrade aqlm
+    elif dpkg --compare-versions "$VERSION_ID" "<=" "22.04"; then
+        pip3 install --upgrade "scipy>=1.11.3"
+        pip3 install --upgrade aqlm[gpu,cpu] # aqlm GPU requires python >= 3.9, not available at ubuntu 20.04
+    fi
 fi
 
 if [ "$cpu_arch" = "arm64" ]; then
     echo "These packages are arm only."
-    pip3 install onnxruntime
-    pip3 install --upgrade aqlm
     case $CUDA_VERSION in
     11.2)
         pip3 install torch==1.12.0 torchaudio==0.12.0 torchvision==0.13.0 -f https://download.pytorch.org/whl/torch_stable.html
@@ -78,12 +83,16 @@ if [ "$cpu_arch" = "arm64" ]; then
         exit 255
         ;;
     esac
+    pip3 install onnxruntime
+    pip3 install --upgrade aqlm
 fi
 
 pip3 install --upgrade keras mxnet opencv-python librosa tensorflow tensorboard tensorboardX imbalanced-learn streamlit seaborn yellowbrick nltk gradio chromadb accelerate
 pip3 install --upgrade torchdiffeq torchsde transformers safetensors tomesd pytorch-lightning einops inflection kornia langchain
 pip3 install --upgrade sentencepiece tiktoken einops optimum wandb
 
+aria2c  -x 16 -j 16  "https://ollama.com/download/ollama-linux-$cpu_arch" -d / -o /usr/bin/ollama
+chmod 755 /usr/bin/ollama
 #pip3 install jupyterlab-tensorboard-pro
 #jupyter serverextension enable jupyter_tensorboard --sys-prefix
 
