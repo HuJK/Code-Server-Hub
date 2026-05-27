b6fc07b408dfd4cc94ff470f89773801df687286
Your Name <you@example.com>
2026-05-27 15:44:12 +0800

    update perm, jupyterhub config path

--- util/jupyterhub_workdir/jupyterhub_config.py
+++ util/jupyterhub_workdir/jupyterhub_config.py
@@ -6,6 +6,6 @@ c.Spawner.default_url = '/lab'
 c.Spawner.environment = {
     "JUPYTER_CONFIG_DIR": lambda spawner: f"/etc/code-server-hub/util/jupyterhub_workdir/jupyter_conf/{spawner.user.name}",
 }
-c.FileCheckpoints.checkpoint_dir = lambda spawner: f"/tmp/.jupyter/checkpoint/{spawner.user.name}"
+c.FileContentsManager.checkpoints_kwargs = {'root_dir': '/tmp/jupyter_checkpoints'}
 c.PAMAuthenticator.open_sessions = True
 c.Authenticator.allow_all = True
